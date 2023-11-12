54ca8f787c7efa5fe07d6ceb83ad0e371cdb02b5
krozv <krozv.code@gmail.com>
2023-11-13 02:14:25 +0900

    no message

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,4 +1,4 @@
-const chosenImage = Math.ceil(Math.random()*14); //15는 directory안에 있는 파일 개수를 자의로 입력한 것
+const chosenImage = Math.ceil(Math.random()*3); //3는 directory안에 있는 파일 개수를 자의로 입력한 것
 
 url = `img/file${chosenImage}.jpg`; //img url
 
